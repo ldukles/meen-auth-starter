8a8cde3af94cdb028aa7242a49d3624d6c430ede
[Lindsey Dukles] <[lindseydukles@gmail.com]>
2022-05-26 15:14:30 -0400

    database set up

--- server.js
+++ server.js
@@ -2,6 +2,21 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+// configure database
+const mongoose = require('mongoose');
+
+// database configuration
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Database Connection Error / Success
+const db = mongoose.connection;
+db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
 
 // LISTENER
 const PORT = process.env.PORT;
